41ac7696ae4149dfd1d7501f0162dc641c86f0d6
akhomlyuk <relicbs@gmail.com>
2023-07-30 10:52:00 +0300

    v0.3 added errors handler, minor fixes

--- handlers/photo_info.py
+++ handlers/photo_info.py
@@ -12,9 +12,13 @@ async def bot_get_photo_info(message: Message):
     json_data = parse_file_to_json(message, 'photo')
     if message.from_user.id != 539491282:
         logging.info(message.from_user)
-    photo_id = message.photo[0].file_id
-    file_info = await conf.bot.get_file(photo_id)
-    await message.answer(f'<b>Photo id:</b> <code>{file_info.file_id}</code>', parse_mode='HTML')
-    await message.answer(f'<b>Photo size:</b> {file_info.file_size // 1024} Kb', parse_mode='HTML')
-    await message.answer(f'<b>Photo unique id:</b> <code>{file_info.file_unique_id}</code>', parse_mode='HTML')
-    await conf.bot.send_document(message.chat.id, json_data)
+    try:
+        photo_id = message.photo[-1].file_id
+        file_info = await conf.bot.get_file(photo_id)
+        await message.answer(f'<b>Photo id:</b> <code>{file_info.file_id}</code>', parse_mode='HTML')
+        await message.answer(f'<b>Photo size:</b> {file_info.file_size // 1024} Kb', parse_mode='HTML')
+        await message.answer(f'<b>Photo unique id:</b> <code>{file_info.file_unique_id}</code>', parse_mode='HTML')
+        await conf.bot.send_document(message.chat.id, json_data)
+    except Exception as e:
+        logging.warning(e)
+        await message.answer(f'{e}')

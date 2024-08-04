1016eff216fbd45e95c87529a4905e731b3fa017
akhomlyuk <relicbs@gmail.com>
2024-08-04 20:57:50 +0300

    info added

--- texts/desc.py
+++ texts/desc.py
@@ -2,5 +2,6 @@ show_info = f'''<b>How To</b>
 Send any file(sticker, photo, etc) to the bot and it will show file ID.
 
 !help or help for info
-Coded with ❤️ on 🐍 and aiogram
+Coded with ❤️
+<b>Channel</b>: @pt_soft
 <b>Author</b>: @exited3n'''
